580211a21444c98a1aeeded6d220a58e5f1cf4d1
putridelfi <pdelfi53@gmail.com>
2015-06-07 18:54:56 +0700

    Update TesInput.java

--- src/tb/TesInput.java
+++ src/tb/TesInput.java
@@ -8,83 +8,291 @@ package TB;
 *
 * @author flora fauna
 */
-abstract public class TesInput extends ControlInput {
+ public class TesInput extends ControlInput {
 
-public TesInput (String namaruang,String lokasiruang,String fakultas,int panjang,
-int lebar,int jumlahkursi,int jumlahpintu,int jumlahjendela,int luas,int rasio,
-int bentuk,int jumlahstopkontak,String kondisistopkontak,String posisistopkontak,
-int jumlahkabellcd,String kondisikabellcd,String posisikabellcd,int jumlahlampu,
-String kondisilampu,String posisilampu,int jumlahkipasangin,String kondisikipasangin,
-String posisikipasangin,int jumlahAC,String kondisiAC,String posisiAC,String pilihSSID,int bandwidth,
-int jumlahCCTV,String kondisiCCTV,String posisiCCTV,String kondisilantai,
-String kondisidinding,String kondisiatap,String kondisipintu,String kondisijendela,
-String sirkulasiudara,int pencahayaan,int kelembapan,int suhu,
-String kebisingan,String bau,String kebocoran,String kerusakan,String keausan,String kekokohan,String kuncipintudanjendela,String bahaya){
+    @Override
+    double hitungluas(int panjang, int lebar) {
+        return super.hitungluas(getPanjang(),getLebar()); //To change body of generated methods, choose Tools | Templates.
+    }
 
-    super(namaruang, lokasiruang, fakultas, bau, panjang, lebar, jumlahkursi, jumlahpintu, jumlahjendela, luas, rasio, bentuk, jumlahstopkontak, kondisistopkontak, posisistopkontak, jumlahkabellcd, kondisikabellcd, posisikabellcd, jumlahlampu, kondisilampu, posisilampu, jumlahkipasangin, kondisikipasangin, posisikipasangin, jumlahAC, kondisiAC, posisiAC, pilihSSID, bandwidth, jumlahCCTV, kondisiCCTV, posisiCCTV, kondisilantai, kondisidinding, kondisiatap, kondisipintu, kondisijendela, sirkulasiudara, pencahayaan, kelembapan, suhu, kebisingan, bau, kebocoran, kerusakan, keausan, kekokohan, kuncipintudanjendela, bahaya);
+//    @Override
+//    double hitungbentukruang(int panjang, int luas) {
+//        return super.hitungbentukruang(panjang, luas); //To change body of generated methods, choose Tools | Templates.
+//    }
+
+    @Override
+    double hitungrasio( int luas, int jumlahkursi) {
+        return super.hitungrasio(luas, jumlahkursi); //To change body of generated methods, choose Tools | Templates.
+    }
+
+//    @Override
+//    double hitungrasioluas(int rasio, double hitungrasioLuas) {
+//        return super.hitungrasioluas(rasio, hitungrasioLuas); //To change body of generated methods, choose Tools | Templates.
+//    }
+     
+     
+
+    @Override
+    double Analisisbau(double Analisisbau) {
+        return super.Analisisbau(Analisisbau); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    double AnalisisjumlahAC(int jumlahAC, double AnalisisjumlahAC) {
+        return super.AnalisisjumlahAC(jumlahAC, AnalisisjumlahAC); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    double AnalisisjumlahCCTV(int jumlahCCTV, double AnalisisjumlahCCTV) {
+        return super.AnalisisjumlahCCTV(jumlahCCTV, AnalisisjumlahCCTV); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    double Analisisjumlahjendela(int jumlahjendela, double Analisisjumlahjendela) {
+        return super.Analisisjumlahjendela(jumlahjendela, Analisisjumlahjendela); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    double Analisisjumlahkabellcd(int jumlahkabellcd, double Analisisjumlahkabellcd) {
+        return super.Analisisjumlahkabellcd(jumlahkabellcd, Analisisjumlahkabellcd); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    double Analisisjumlahkipasangin(int jumlahkipasangin, double Analisisjumlahkipasangin) {
+        return super.Analisisjumlahkipasangin(jumlahkipasangin, Analisisjumlahkipasangin); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    double Analisisjumlahlampu(int jumlahlampu, double Analisisjumlahlampu) {
+        return super.Analisisjumlahlampu(jumlahlampu, Analisisjumlahlampu); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    double Analisisjumlahpintu(int jumlahpintu, double Analisisjumlahpintu) {
+        return super.Analisisjumlahpintu(jumlahpintu, Analisisjumlahpintu); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    double Analisisjumlahstopkontak(int jumlahstopkontak, double Analisisjumlahstopkontak) {
+        return super.Analisisjumlahstopkontak(jumlahstopkontak, Analisisjumlahstopkontak); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    double Analisiskeamananruang(double Analisiskeamananruang) {
+        return super.Analisiskeamananruang(Analisiskeamananruang); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    double Analisiskeausan(double Analisiskeausan) {
+        return super.Analisiskeausan(Analisiskeausan); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    double Analisiskebersihan(int b, double Analsiskebersihan) {
+        return super.Analisiskebersihan(b, Analsiskebersihan); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    double Analisiskebisingan(double Analisiskebisingan) {
+        return super.Analisiskebisingan(Analisiskebisingan); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    double Analisiskebocoran(double Analisiskebocoran) {
+        return super.Analisiskebocoran(Analisiskebocoran); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    double Analisiskekokohan(double Analisiskekokohan) {
+        return super.Analisiskekokohan(Analisiskekokohan); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    double Analisiskelembapan(int kelembapan, double Analisiskelembapan) {
+        return super.Analisiskelembapan(kelembapan, Analisiskelembapan); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    double Analisiskerusakan(double Analisiskerusakan) {
+        return super.Analisiskerusakan(Analisiskerusakan); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    double AnalisiskondisiAC(int jumlahAC, String kondisiAC, double AnalisiskondisiAC) {
+        return super.AnalisiskondisiAC(jumlahAC, kondisiAC, AnalisiskondisiAC); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    double AnalisiskondisiCCTV(String kondisiCCTV, double AnalisiskondisiCCTV) {
+        return super.AnalisiskondisiCCTV(kondisiCCTV, AnalisiskondisiCCTV); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    double Analisiskondisikabellcd(String kondisikabellcd, double Analisiskondisikabellcd) {
+        return super.Analisiskondisikabellcd(kondisikabellcd, Analisiskondisikabellcd); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    double Analisiskondisikipasangin(int jumlahkipasangin, String kondisikipasangin, double Analisiskondisikipasangin) {
+        return super.Analisiskondisikipasangin(jumlahkipasangin, kondisikipasangin, Analisiskondisikipasangin); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    double Analisiskondisilampu(String kondisilampu, double Analisiskondisilampu) {
+        return super.Analisiskondisilampu(kondisilampu, Analisiskondisilampu); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    double Analisiskondisistopkontak(int jumlahstopkontak, String kondisistopkontak, double Analisiskondisistopkontak) {
+        return super.Analisiskondisistopkontak(jumlahstopkontak, kondisistopkontak, Analisiskondisistopkontak); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    double Analisiskuncipintudanjendela(double Analisiskuncipintudanjendela) {
+        return super.Analisiskuncipintudanjendela(Analisiskuncipintudanjendela); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    double Analisislogininternet(String pilihSSID, double Analisislogininternet) {
+        return super.Analisislogininternet(pilihSSID, Analisislogininternet); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    double Analisispencahayaan(int pencahayaan, double Analisispencahayaan) {
+        return super.Analisispencahayaan(pencahayaan, Analisispencahayaan); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    double AnalisisposisiAC(String posisiAC, double AnalisisposisiAC) {
+        return super.AnalisisposisiAC(posisiAC, AnalisisposisiAC); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    double AnalisisposisiCCTV(String posisiCCTV, double AnalisisposisiCCTV) {
+        return super.AnalisisposisiCCTV(posisiCCTV, AnalisisposisiCCTV); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    double Analisisposisikabellcd(String posisikabellcd, double Analisisposisikabellcd) {
+        return super.Analisisposisikabellcd(posisikabellcd, Analisisposisikabellcd); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    double Analisisposisikipasangin(String posisikipasangin, double Analisisposisikipasangin) {
+        return super.Analisisposisikipasangin(posisikipasangin, Analisisposisikipasangin); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    double Analisisposisilampu(String posisilampu, double Analisisposisilampu) {
+        return super.Analisisposisilampu(posisilampu, Analisisposisilampu); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    double Analisisposisistopkontak(String posisistopkontak, double Analisisposisistopkontak) {
+        return super.Analisisposisistopkontak(posisistopkontak, Analisisposisistopkontak); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    double Analisissirkulasiudara(String sirkulasiudara, double Analisissirkulasiudara) {
+        return super.Analisissirkulasiudara(sirkulasiudara, Analisissirkulasiudara); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    double Analisissuhu(int suhu, double Analisissuhu) {
+        return super.Analisissuhu(suhu, Analisissuhu); //To change body of generated methods, choose Tools | Templates.
+    }
     
-       
-    super.hitungluas(panjang, lebar, luas);
-    super.hitungrasio(rasio, luas, jumlahkursi);
-    super.hitungbentukruang(panjang, luas, bentuk);
-    super.hitungrasioluas(rasio, rasio);
-    
-    super.Analisisjumlahpintu(jumlahpintu, jumlahpintu);
-    super.Analisisjumlahjendela(jumlahjendela, jumlahjendela);
-            
-    super.Analisisjumlahstopkontak(jumlahstopkontak, jumlahstopkontak);
-    super.Analisiskondisistopkontak(jumlahstopkontak, kondisistopkontak, jumlahstopkontak);
-    super.Analisisposisistopkontak(posisistopkontak, jumlahstopkontak);
-        
-    super.Analisisjumlahkabellcd(jumlahkabellcd, jumlahkabellcd);
-    super.Analisiskondisikabellcd(kondisikabellcd, jumlahkabellcd);
-    super.Analisisposisikabellcd(posisikabellcd, jumlahkabellcd);
+
+  
    
-    super.Analisisjumlahlampu(jumlahlampu, jumlahlampu);
-    super.Analisiskondisilampu(kondisilampu, jumlahlampu);
-    super.Analisisposisilampu(posisilampu, jumlahlampu);
+
+//public TesInput (String namaruang,String lokasiruang,String fakultas,int panjang,
+//int lebar,int jumlahkursi,int jumlahpintu,int jumlahjendela,int luas,int rasio,
+//int bentuk,int jumlahstopkontak,String kondisistopkontak,String posisistopkontak,
+//int jumlahkabellcd,String kondisikabellcd,String posisikabellcd,int jumlahlampu,
+//String kondisilampu,String posisilampu,int jumlahkipasangin,String kondisikipasangin,
+//String posisikipasangin,int jumlahAC,String kondisiAC,String posisiAC,String pilihSSID,int bandwidth,
+//int jumlahCCTV,String kondisiCCTV,String posisiCCTV,String kondisilantai,
+//String kondisidinding,String kondisiatap,String kondisipintu,String kondisijendela,
+//String sirkulasiudara,int pencahayaan,int kelembapan,int suhu,
+//String kebisingan,String bau,String kebocoran,String kerusakan,String keausan,String kekokohan,String kuncipintudanjendela,String bahaya){
+//
+//    super(namaruang, lokasiruang, fakultas, bau, panjang, lebar, jumlahkursi, jumlahpintu, jumlahjendela, luas, rasio, bentuk, jumlahstopkontak, kondisistopkontak, posisistopkontak, jumlahkabellcd, kondisikabellcd, posisikabellcd, jumlahlampu, kondisilampu, posisilampu, jumlahkipasangin, kondisikipasangin, posisikipasangin, jumlahAC, kondisiAC, posisiAC, pilihSSID, bandwidth, jumlahCCTV, kondisiCCTV, posisiCCTV, kondisilantai, kondisidinding, kondisiatap, kondisipintu, kondisijendela, sirkulasiudara, pencahayaan, kelembapan, suhu, kebisingan, bau, kebocoran, kerusakan, keausan, kekokohan, kuncipintudanjendela, bahaya);
+
+
+          
+//    super.hitungluas(panjang, lebar, luas);
+//    super.hitungrasio(rasio, luas, jumlahkursi);
+//    super.hitungbentukruang(panjang, luas, bentuk);
+//    super.hitungrasioluas(rasio, rasio);
+//    
+//    super.Analisisjumlahpintu(jumlahpintu, jumlahpintu);
+//    super.Analisisjumlahjendela(jumlahjendela, jumlahjendela);
+//            
+//    super.Analisisjumlahstopkontak(jumlahstopkontak, jumlahstopkontak);
+//    super.Analisiskondisistopkontak(jumlahstopkontak, kondisistopkontak, jumlahstopkontak);
+//    super.Analisisposisistopkontak(posisistopkontak, jumlahstopkontak);
+//        
+//    super.Analisisjumlahkabellcd(jumlahkabellcd, jumlahkabellcd);
+//    super.Analisiskondisikabellcd(kondisikabellcd, jumlahkabellcd);
+//    super.Analisisposisikabellcd(posisikabellcd, jumlahkabellcd);
+//   
+//    super.Analisisjumlahlampu(jumlahlampu, jumlahlampu);
+//    super.Analisiskondisilampu(kondisilampu, jumlahlampu);
+//    super.Analisisposisilampu(posisilampu, jumlahlampu);
+//    
+//    super.Analisisjumlahkipasangin(jumlahkipasangin, jumlahpintu);
+//    super.Analisiskondisikipasangin(jumlahkipasangin, kondisikipasangin, jumlahkipasangin);
+//    super.Analisisposisikipasangin(posisikipasangin, jumlahkipasangin);
+//    
+//    super.AnalisisjumlahAC(jumlahAC, jumlahAC);
+//    super.AnalisiskondisiAC(jumlahAC, kondisiAC, rasio);
+//    super.AnalisisposisiAC(posisiAC, rasio);
+//    
+//    super.Analisisinternet(pilihSSID, jumlahpintu);
+//    super.Analisislogininternet(pilihSSID, jumlahstopkontak);
+//    
+//    super.AnalisisjumlahCCTV(jumlahCCTV, jumlahCCTV);
+//    super.AnalisiskondisiCCTV(kondisiCCTV, jumlahCCTV);
+//    super.AnalisisposisiCCTV(posisiCCTV, jumlahCCTV);
+//    
+//    super.Analisiskebersihan(suhu, kelembapan);
+//    super.Analisissirkulasiudara(sirkulasiudara, jumlahkursi);
+//    super.Analisispencahayaan(pencahayaan, pencahayaan);
+//    super.Analisiskelembapan(kelembapan, kelembapan);
+//    super.Analisissuhu(suhu, suhu);
+//    super.Analisiskebisingan(jumlahkipasangin);
+//    super.Analisisbau(rasio);
+//    super.Analisiskebocoran(kelembapan);
+//    super.Analisiskerusakan(kelembapan);
+//    super.Analisiskeausan(kelembapan);
+//    
+//    super.Analisiskekokohan(kelembapan);
+//    super.Analisiskuncipintudanjendela(jumlahjendela);
+//    super.Analisiskeamananruang(kelembapan);  
+//    
+//    super.Analisissuhu(suhu, suhu);
+//    super.hitungbentukruang(panjang, luas, bentuk);
+//    super.hitungrasioluas(rasio, rasio);
+//    super.Analisiskeausan(rasio);
+//    super.Analisiskekokohan(kelembapan);
+//    super.Analisiskuncipintudanjendela(jumlahjendela);
+//    
+//    super.Analisiskebisingan(kelembapan);
+//    super.Analisiskebocoran(kelembapan);
+//    super.Analisiskerusakan(kelembapan);
+//      
+//}
+
+    @Override
+    double Analisisinternet(String pilihSSID, double Analisisinternet) {
+        return super.Analisisinternet(pilihSSID, Analisisinternet); //To change body of generated methods, choose Tools | Templates.
+    }
+
     
-    super.Analisisjumlahkipasangin(jumlahkipasangin, jumlahpintu);
-    super.Analisiskondisikipasangin(jumlahkipasangin, kondisikipasangin, jumlahkipasangin);
-    super.Analisisposisikipasangin(posisikipasangin, jumlahkipasangin);
     
-    super.AnalisisjumlahAC(jumlahAC, jumlahAC);
-    super.AnalisiskondisiAC(jumlahAC, kondisiAC, rasio);
-    super.AnalisisposisiAC(posisiAC, rasio);
     
-    super.Analisisinternet(pilihSSID, jumlahpintu);
-    super.Analisislogininternet(pilihSSID, jumlahstopkontak);
     
-    super.AnalisisjumlahCCTV(jumlahCCTV, jumlahCCTV);
-    super.AnalisiskondisiCCTV(kondisiCCTV, jumlahCCTV);
-    super.AnalisisposisiCCTV(posisiCCTV, jumlahCCTV);
     
-    super.Analisiskebersihan(suhu, kelembapan);
-    super.Analisissirkulasiudara(sirkulasiudara, jumlahkursi);
-    super.Analisispencahayaan(pencahayaan, pencahayaan);
-    super.Analisiskelembapan(kelembapan, kelembapan);
-    super.Analisissuhu(suhu, suhu);
-    super.Analisiskebisingan(jumlahkipasangin);
-    super.Analisisbau(rasio);
-    super.Analisiskebocoran(kelembapan);
-    super.Analisiskerusakan(kelembapan);
-    super.Analisiskeausan(kelembapan);
-    
-    super.Analisiskekokohan(kelembapan);
-    super.Analisiskuncipintudanjendela(jumlahjendela);
-    super.Analisiskeamananruang(kelembapan);  
-    
-    super.Analisissuhu(suhu, suhu);
-    super.hitungbentukruang(panjang, luas, bentuk);
-    super.hitungrasioluas(rasio, rasio);
-    super.Analisiskeausan(rasio);
-    super.Analisiskekokohan(kelembapan);
-    super.Analisiskuncipintudanjendela(jumlahjendela);
-    
-    super.Analisiskebisingan(kelembapan);
-    super.Analisiskebocoran(kelembapan);
-    super.Analisiskerusakan(kelembapan);
-      
-}
 }
 
